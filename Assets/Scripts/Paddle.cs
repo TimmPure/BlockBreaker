2cbc6c36df76048c60172ac6501969295f03fea8
TimmPure <T.j.m.voskuil@gmail.com>
2020-08-05 17:39:13 +0200

    Move Paddle to Mouse X position

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    void Update() {
+        movePaddleToMouseX();
+    }
+
+    private void movePaddleToMouseX() {
+        float mouseXPosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(mouseXPosInUnits, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
